26c734f72644cb30b71d7d3f9a7ba7402e2984d8
tommo@fastmail.com <>
2022-12-13 15:12:29 +1000

    complete challenge 03 step 01

--- 03/main.go
+++ 03/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 )
 
 func main() {
@@ -13,13 +14,38 @@ func main() {
 	// get splits
 	for _, l := range f {
 		ll := len(l)
-		c := ll / 2
-		pack = append(pack, []string{l[c:], l[:(ll - c)]})
+		if ll > 0 {
+			c := ll / 2
+			pack = append(pack, []string{l[c:], l[:(ll - c)]})
+		}
 	}
 	fmt.Println(pack)
-	fmt.Println(toAlphaIndex([]rune("a")[0]))
-	fmt.Println(toAlphaIndex([]rune("b")[0]))
 
+	// test outputs
+	fmt.Println(toAlphaIndex([]rune("a")[0]))
+	fmt.Println(toAlphaIndex([]rune("A")[0]))
+
+	// run problem space
+	var score int
+	for _, p := range pack {
+		// dummy map for unique matches
+		found := make(map[string]interface{})
+		for _, i := range p[0] {
+			s := string(i)
+			if strings.Contains(p[1], s) {
+				if _, ok := found[s]; !ok {
+					// register the string as found in the map
+					found[s] = nil
+				}
+			}
+		}
+		// score our map
+		for k := range found {
+			score += toAlphaIndex([]rune(k)[0])
+		}
+
+	}
+	fmt.Printf("Final score is: %d", score)
 }
 
 func loadfl(f string) []string {
@@ -33,5 +59,11 @@ func loadfl(f string) []string {
 
 // convert to ascii number
 func toAlphaIndex(r rune) int {
-	return int(r-'0') - 48
+	i := int(r - '0')
+	// for lowercase: offset to 1 index and return
+	if !unicode.IsUpper(r) {
+		return i - 48
+	}
+	// for uppercase: offset to 27 index and return
+	return (i) + 10
 }
